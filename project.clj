a48d0cfa38a624e6efcbc4b132e6e4eb5715b452
Paul Evans <evansp2@gmail.com>
2015-11-12 14:36:21 -0500

    Version 0.0.26

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pe-fp-core "0.0.26-SNAPSHOT"
+(defproject pe-fp-core "0.0.26"
   :description "A Clojure library encapsulating the various domain objects associated with the Gas Jot application."
   :url "https://github.com/evanspa/pe-fp-core"
   :license {:name "MIT"
