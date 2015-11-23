6978a3043a7abed03783c6d9001412bfaed059ad
Paul Evans <evansp2@gmail.com>
2015-11-23 10:22:11 -0500

    updating to use latest deps

--- project.clj
+++ project.clj
@@ -16,10 +16,10 @@
                  [com.cemerick/friend "0.2.1"]
                  [pe-core-utils "0.0.11"]
                  [pe-jdbc-utils "0.0.18"]
-                 [pe-user-core "0.1.36"]]
+                 [pe-user-core "0.1.38"]]
   :resource-paths ["resources"]
   :codox {:exclude [user]
-          :src-dir-uri "https://github.com/evanspa/pe-gasjot-core/blob/0.0.27/"
+          :src-dir-uri "https://github.com/evanspa/pe-gasjot-core/blob/0.0.28/"
           :src-linenum-anchor-prefix "L"}
   :profiles {:dev {:source-paths ["dev"]  ;ensures 'user.clj' gets auto-loaded
                    :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
